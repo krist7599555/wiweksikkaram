943d94c4e0121daa0b6650cdcd0ae12c6e05b6a7
Krist Ponpairin <krist7599555@gmail.com>
2025-11-18 12:53:13 +0700

    refactor: extract part of home to post/index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,11 @@ require "net/http"
 require "json"
 
 class PagesController < ApplicationController
-  include ActionView::Helpers::AssetUrlHelper
-  include Rails.application.routes.url_helpers
   def home
-    file_path = Rails.root.join("app", "assets", "dekdee.json")
-    json_data_string = File.read(file_path)
-    parsed_data = JSON.parse(json_data_string)
-    @posts = parsed_data
+    # TODO: duplicate to ./post_controller.rb#index
+    @posts = Rails
+      .public_path.join("dekd", "index.json")
+      .then { |it| File.read it }
+      .then { |it| JSON.parse it }
   end
 end
--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,9 +1,35 @@
+require "nokogiri"
+
 class PostController < ApplicationController
   def index
+    @posts = Rails
+      .public_path.join("dekd", "index.json")
+      .then { |it| File.read it }
+      .then { |it| JSON.parse it }
   end
 
   def show
-    @id = params[:id]
-    @post = DekdUtils.fetch_post(@id)
+    chapter = params[:id]
+    @post = Rails
+      .public_path.join("dekd", "chapter", "#{chapter}.html")
+      .then { |it| File.read it }
+      .then { |it| Nokogiri::HTML it }
+      .then { |doc|
+        {
+          chapter: chapter,
+          url: "https://writer.dek-d.com/dekdee/writer/viewlongc.php?id=2485901&chapter=#{chapter}",
+          book: doc.at_css(".chapter-box-wrapper .novel-name")&.text,
+          title: doc.at_css(".chapter-box-wrapper .chaptername")&.text,
+          date: doc.at_css(".chapter-box-wrapper .chapter-metainfo-wrapper")&.text,
+          content: doc
+            .css("#story-content p")
+            .map { |p|
+              ("<p>" + p.text + "</p>")
+              .gsub!("&ZeroWidthSpace;", "")
+              .tr!("๰๪๵๲๯๭๳๮๥๦๨๹๸๴๷๫๱๬", "ฌฆฑฏซฉฏชกขคตดฐณงญจ")
+            }
+            .join
+        }
+      }
   end
 end
